49e0d6e1b39958e036755460bc1ae13f6043de4f
ma3722 <ma3722@ic.ac.uk>
2023-05-30 12:49:11 +0100

    Load file and template to process into words

--- src/emulate.c
+++ src/emulate.c
@@ -1,35 +1,81 @@
+#include <stdio.h>
 #include <stdlib.h>
+#define WORD_SIZE 32
 
 int main(int argc, char **argv) {
-  return EXIT_SUCCESS;
-}
+    FILE *file;
+    unsigned char *buffer;
 
-
-#include <stdio.h>
-#include <stdint.h>
-
-#define WORD_SIZE 4 // 32 bits (4 bytes)
-
-int main() {
-    FILE *file = fopen("binary_file.bin", "rb"); // Open binary file for reading
-
+    if (argc != 2) {
+        return EXIT_FAILURE;
+    }
+    
+    // Open file
+    file = fopen(argv[1], "rb");
     if (file == NULL) {
-        printf("Failed to open the file.\n");
-        return 1;
+        printf("Opening file failed.\n");
+        return EXIT_FAILURE;
     }
-
-    fseek(file, 0, SEEK_SET); // Seek to the beginning of the file
-
-    uint8_t buffer[WORD_SIZE]; // Buffer to hold a word (32-bit instruction)
-
-    while (fread(buffer, WORD_SIZE, 1, file) == 1) {
-        // Process each word in the buffer
-        // You can access the individual bytes of the word using buffer[0], buffer[1], buffer[2], buffer[3]
-        // Do whatever you need to do with the word (e.g., print it, analyze it, etc.)
+    
+    // File size
+    fseek(file, 0, SEEK_END);
+    long fileSize = ftell(file);
+    rewind(file);
+    
+    // Allocate memory for buffer
+    buffer = (unsigned char*)malloc(fileSize);
+    if (buffer == NULL) {
+        printf("Memory allocation failed.\n");
+        fclose(file);
+        return EXIT_FAILURE;
     }
+    
+    // Read file into buffer
+    size_t bytesRead = fread(buffer, 1, fileSize, file);
+    if (bytesRead != fileSize) {
+        printf("Failed to read file.\n");
+        free(buffer);
+        fclose(file);
+        return EXIT_FAILURE;
+    }
+    
+    // Close file
+    fclose(file);
 
-    fclose(file); // Close the file
+    // no. words
+    int numWords = fileSize / sizeof(unsigned int);
+    
+    // process each word
+    for (int i = 0; i < numWords; i++) {
+      unsigned int word = *(unsigned int *)(buffer + i * sizeof(unsigned int));
+      unsigned char bitArray[WORD_SIZE];
+      
+      for (int j = 0; j < WORD_SIZE; j++) {
+        bitArray[j] = (word >> j) & 1;
+      }
+    }
+    // Print byte in hex
+    //for (long i = 0; i < fileSize; i++) {
+    //    printf("%08x ", buffer[i]); 
+    //}
 
+    // Print in binary
+    // for (long i = 0; i < fileSize; i++) {
+        
+    //     for (int j = 7; j >= 0; j--) {
+    //         unsigned char mask = 1 << j;
+    //         unsigned char bit = (buffer[i] & mask) ? 1 : 0;
+    //         printf("%d", bit);
+    //     }
+    //     printf(" ");
+    // }
+    // printf("\n");
+    // printf("%ld\n", fileSize / 4);
+    // printf("%ld\n", fileSize % 4);
+    
+    
+    free(buffer);
+    
     return 0;
 }
 
