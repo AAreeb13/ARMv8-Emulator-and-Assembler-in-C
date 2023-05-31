10a606799170fd46f72d3c196b74288aeff125fd
ma3722 <ma3722@ic.ac.uk>
2023-05-31 12:56:23 +0100

    modified to malloc, added getBit and printByte

--- src/emulate.c
+++ src/emulate.c
@@ -1,81 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define WORD_SIZE 32
+#define MEMORY_SIZE 2097152
+
+
+unsigned char getBit(unsigned int, int);
+void printByte(unsigned char);
+
+unsigned char *memory;
 
 int main(int argc, char **argv) {
-    FILE *file;
-    unsigned char *buffer;
+  FILE *file;
+  // Make sure that there is a second parameter(binary file)
+  if (argc != 2) {
+    return EXIT_FAILURE;
+  }
 
-    if (argc != 2) {
-        return EXIT_FAILURE;
-    }
-    
-    // Open file
-    file = fopen(argv[1], "rb");
-    if (file == NULL) {
-        printf("Opening file failed.\n");
-        return EXIT_FAILURE;
-    }
-    
-    // File size
-    fseek(file, 0, SEEK_END);
-    long fileSize = ftell(file);
-    rewind(file);
-    
-    // Allocate memory for buffer
-    buffer = (unsigned char*)malloc(fileSize);
-    if (buffer == NULL) {
-        printf("Memory allocation failed.\n");
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-    
-    // Read file into buffer
-    size_t bytesRead = fread(buffer, 1, fileSize, file);
-    if (bytesRead != fileSize) {
-        printf("Failed to read file.\n");
-        free(buffer);
-        fclose(file);
-        return EXIT_FAILURE;
-    }
-    
-    // Close file
+  // Memory is given 2MB
+  memory = (unsigned char *) malloc(MEMORY_SIZE);
+  if (memory == NULL) {
+    printf("Memory allocation failed.\n");
+    return EXIT_FAILURE;
+  }
+  // Open file
+  file = fopen(argv[1], "rb");
+  if (file == NULL) {
+    printf("Opening file failed.\n");
+    return EXIT_FAILURE;
+  }
+
+  // File size
+  fseek(file, 0, SEEK_END);
+  long fileSize = ftell(file);
+  rewind(file);
+
+  // Read file into memory
+  size_t bytesRead = fread(memory, 1, fileSize, file);
+  if (bytesRead != fileSize) {
+    printf("Failed to read file.\n");
+    free(memory);
     fclose(file);
+    return EXIT_FAILURE;
+  }
 
-    // no. words
-    int numWords = fileSize / sizeof(unsigned int);
-    
-    // process each word
-    for (int i = 0; i < numWords; i++) {
-      unsigned int word = *(unsigned int *)(buffer + i * sizeof(unsigned int));
-      unsigned char bitArray[WORD_SIZE];
-      
-      for (int j = 0; j < WORD_SIZE; j++) {
-        bitArray[j] = (word >> j) & 1;
-      }
-    }
-    // Print byte in hex
-    //for (long i = 0; i < fileSize; i++) {
-    //    printf("%08x ", buffer[i]); 
-    //}
+  // Close file
+  fclose(file);
 
-    // Print in binary
-    // for (long i = 0; i < fileSize; i++) {
-        
-    //     for (int j = 7; j >= 0; j--) {
-    //         unsigned char mask = 1 << j;
-    //         unsigned char bit = (buffer[i] & mask) ? 1 : 0;
-    //         printf("%d", bit);
-    //     }
-    //     printf(" ");
-    // }
-    // printf("\n");
-    // printf("%ld\n", fileSize / 4);
-    // printf("%ld\n", fileSize % 4);
-    
-    
-    free(buffer);
-    
-    return 0;
+
+
+  // no. words
+  // int numWords = fileSize / sizeof(unsigned int);
+
+  // // process each word
+  // for (int i = 0; i < numWords; i++) {
+  //   unsigned int word = *(unsigned int *)(memory + i * sizeof(unsigned int));
+  //   unsigned char bitArray[WORD_SIZE];
+
+  //   for (int j = 0; j < WORD_SIZE; j++) {
+  //     bitArray[j] = (word >> j) & 1;
+  //   }
+  // }
+  // Print byte in hex
+  // for (long i = 0; i < fileSize; i++) {
+  //    printf("%08x ", memory[i]);
+
+  free(memory);
+  return 0;
 }
 
+// @param i - ensure it's between 0 and 31 inclusive.
+unsigned char getBit(unsigned int word, int i) {
+  return (word >> i) & 1;
+}
+
+// @param byte - pass in memory[i].
+void printByte(unsigned char byte) {
+  for (int j = 7; j >= 0; j--) {
+    unsigned char mask = 1 << j;
+    unsigned char bit = (byte & mask) ? 1 : 0;
+    printf("%d", bit);
+  }
+}
