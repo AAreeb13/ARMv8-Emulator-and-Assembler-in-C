dd9db429e0c0831a51b3ca6af7f1fe0e55639192
ma3722 <ma3722@ic.ac.uk>
2023-05-31 19:45:58 +0100

    data process imm arith case (incomplete)

--- src/emulate.c
+++ src/emulate.c
@@ -9,6 +9,28 @@
 
 #define NUM_REGS 31
 // registers: X00 -> X30
+
+//bit-accessible functions
+unsigned char getBit(unsigned int, int);
+void printByte(unsigned char);
+unsigned int getSubWord(int,int,unsigned int);
+int power(int,int);
+
+// Data Processing (Immediate) functions           NEED TO UPDATE FLAGS!!!
+void dataProcessImmediate(unsigned int);
+void arithmetic(unsigned int, bool);
+void add(unsigned int, unsigned int, unsigned int);
+void addc(unsigned int, unsigned int, unsigned int);
+void sub(unsigned int, unsigned int, unsigned int);
+void subc(unsigned int, unsigned int, unsigned int);
+
+//Single Data Transfer functions
+void singleDataTransfer(unsigned int);
+void unsignedImmediateOffset(unsigned int, int);
+void preIndexed(unsigned int, int);
+void registerOffset(unsigned int, int);
+void loadLiteral(unsigned int, int);
+
 struct Reg {
   char name[4];
   int value; // stands for Xn -> in binary
@@ -36,19 +58,6 @@ struct PState {
 };
 
 struct PState pState = {false, true, false, false};
-
-//bit-accessible functions
-unsigned char getBit(unsigned int, int);
-void printByte(unsigned char);
-unsigned int getSubWord(int,int,unsigned int);
-int power(int,int);
-
-//Single Data Transfer functions
-void singleDataTransfer(unsigned int);
-void unsignedImmediateOffset(unsigned int, int);
-void preIndexed(unsigned int, int);
-void registerOffset(unsigned int, int);
-void loadLiteral(unsigned int, int);
 
 //main memory
 unsigned char *memory;
@@ -107,16 +116,27 @@ int main(int argc, char **argv) {
   // Print byte in hex
   // for (long i = 0; i < fileSize; i++) {
   //    printf("%08x ", memory[i]);
-  printf("%d\n", getSubWord(4,7, 185));
 
   free(memory);
   return 0;
 }
+
+int power( int base, int exponent) {
+  if (exponent == 0) {
+    return 1;
+  } else if (exponent % 2 == 0) {
+    int temp = power(base, exponent / 2);
+    return temp * temp;
+  } else {
+    return base * power(base, exponent - 1);
+  }
+}
+
 //accesses subsection of instruction between indexes start to end, inclusively.
 //PRE: end>=start
 unsigned int getSubWord(int start, int end, unsigned int word){
   int width = end - start + 1;
-  unsigned int subword = (word >> start) & ((int) (power(2,width) - 1));
+  unsigned int subword = (word >> start) & (power(2,width) - 1);
   return subword;
 }
 
@@ -125,13 +145,69 @@ unsigned char getBit(unsigned int word, int i) {
   return (word >> i) & 1;
 }
 
-// @param byte - pass in memory[i].
+// @param byte - for passing in memory[i].
 void printByte(unsigned char byte) {
   for (int j = 7; j >= 0; j--) {
     unsigned char mask = 1 << j;
     unsigned char bit = (byte & mask) ? 1 : 0;
     printf("%d", bit);
   }
+}
+
+void dataProcessImmediate(unsigned int word) {
+  unsigned char opi = getSubWord(23, 25, word);
+  if (opi == 2) {
+    unsigned char sf = getBit(word, 31);
+    if (sf == 1) {
+      arithmetic(word, 1);
+    } else {
+      arithmetic(word, 0);
+    }
+  } else if (opi == 5) {
+  }
+}
+
+void arithmetic(unsigned int word, bool Xn) {
+  unsigned char sh = getBit(word, 22);
+  unsigned int Op_2;
+
+  if (sh == 0) {
+    Op_2 = getSubWord(10, 21, word);
+  } else {
+    Op_2 = getSubWord(10, 21, word) << 12;
+  }
+
+  unsigned char opc = getSubWord(29, 30, word);
+  unsigned char rd = getSubWord(0, 4, word);
+  unsigned char rn = getSubWord(5, 9, word);
+
+
+  if (opc == 0) {
+    add(rn, Op_2, rd);
+  } else if (opc == 1) {
+    addc(rn, Op_2, rd);
+  } else if (opc == 2) {
+    sub(rn, Op_2, rd);
+  } else {
+    subc(rn, Op_2, rd);
+  }
+}
+
+void add(unsigned int rn, unsigned int Op_2, unsigned int rd) {
+  genRegisters[rd].value = genRegisters[rn].value + Op_2;
+  // POSSIBLY UPDATE FLAGS ???
+}
+void addc(unsigned int rn, unsigned int Op_2, unsigned int rd) {
+  genRegisters[rd].value = genRegisters[rn].value + Op_2;
+  // UPDATE FLAGS!!!
+}
+void sub(unsigned int rn, unsigned int Op_2, unsigned int rd) {
+  genRegisters[rd].value = genRegisters[rn].value - Op_2;
+}
+
+void subc(unsigned int rn, unsigned int Op_2, unsigned int rd) {
+  genRegisters[rd].value = genRegisters[rn].value - Op_2;
+  // UPDATE FLAGS!!!
 }
 
 void singleDataTransfer(unsigned int word){
@@ -170,17 +246,4 @@ void registerOffset(unsigned int word,int l){
 }
 void loadLiteral(unsigned int word,int l){
 
-}
-
-
-
-int power( int base, int exponent) {
-    if (exponent == 0) {
-        return 1;
-    } else if (exponent % 2 == 0) {
-        int temp = power(base, exponent / 2);
-        return temp * temp;
-    } else {
-        return base * power(base, exponent - 1);
-    }
 }
