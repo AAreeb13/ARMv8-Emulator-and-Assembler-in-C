7367ede0dcbda5c447c9fadb8cef8701a5ecb667
Ifaz Uddin <iu22@aube.doc.ic.ac.uk>
2023-06-09 19:07:26 +0100

    Fixed issue with arithmetic

--- src/dataProcessImmediate.h
+++ src/dataProcessImmediate.h
@@ -8,7 +8,6 @@ extern void dataProcessImmediate(uint32_t);
 
 extern void arithmetic(uint32_t, bool);
 extern void add(uint8_t, uint32_t, uint8_t, bool);
-extern void addReg(uint8_t, uint64_t, uint8_t, bool);
 extern void adds(uint8_t, uint32_t, uint8_t, bool);
 extern void sub(uint8_t, uint32_t, uint8_t, bool);
 extern void subs(uint8_t, uint32_t, uint8_t, bool);
