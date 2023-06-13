0b42df3998ab8903889420f096067d8a69a9f0ca
ma3722 <ma3722@ic.ac.uk>
2023-06-13 20:18:18 +0100

    how to use putBits

--- src/utils.c
+++ src/utils.c
@@ -1,5 +1,7 @@
 #include "utils.h"
 
+// puts bits in instruction word in desired location
+// @param endIndex - index of last bit in @param bits
 void putBits(uint32_t *total, uint32_t bits, uint32_t endIndex) {
   *total += bits << endIndex;
 }
