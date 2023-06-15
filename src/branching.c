a826def0ff0fd5a72dd16012922db4617e73d718
Suweda Abdirahman <sa1722@aube.doc.ic.ac.uk>
2023-06-15 15:00:22 +0100

    added Mahdi's putBit and replaced my shifts in branch functions with it

--- src/branching.c
+++ src/branching.c
@@ -4,6 +4,7 @@
 
 #include "branching.h"
 #include "structures.h"
+#include "utils.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -11,27 +12,28 @@
 uint32_t unconditionalOffsetA(Node node) {
     // need symbol table to get label address
     uint32_t labelAddress;
-    uint32_t binary;
-    binary += 0b101 << 26;
-    binary += ((labelAddress - node.memoryAddress) / 4) >> 2;
-    return binary;
+    uint32_t result;
+    putBits(&result, 0b101, 26);
+    result += (labelAddress - node.memoryAddress) / 4);
+    return result;
 }
 
 // br xn -> branch to address in Xn (??)
 uint32_t unconditionalRegisterA(Node node) {
-    uint32_t binary;
-    binary += 0b1101011 << 25;
-    binary += 0b11111 << 16;
-    binary += node.args[0] << 25;
-    return binary;
+    uint32_t result;
+    putBits(&result, 0b1101011, 26);
+    putBits(&result, 0b11111, 16);
+    putBits(&result, parseReg(node.args[0], 0, 0), 5);
+    return result;
 }
 
 // b.cond label -> branch to literal iff PSTATE satisfied cond
 uint32_t conditionalBranchesA(Node node, char mnemonic[]) {
     // need symbol table to get label address
     uint32_t labelAddress;
-    uint32_t binary;
+    uint32_t result;
     uint8_t cond;
+
     if (strcmp(mnemonic, "eq")) {cond = 0b0000}
     else if (strcmp(mnemonic, "ne")) {cond = 0b0001}
     else if (strcmp(mnemonic, "ge")) {cond = 0b1010}
@@ -39,7 +41,10 @@ uint32_t conditionalBranchesA(Node node, char mnemonic[]) {
     else if (strcmp(mnemonic, "gt")) {cond = 0b1100}
     else if (strcmp(mnemonic, "le")) {cond = 0b1101}
     else if (strcmp(mnemonic, "al")) {cond = 0b1110}
-    binary += 0b10101 << 26;
-    binary += ((labelAddress - node.memoryAddress) / 4) << 5;
-    binary += cond;
+
+    putBits(&result, 0b10101, 26);
+    putBits(&result, (labelAddress - node.memoryAddress) / 4), 5);
+    putBits(&result, cond, 0);
+
+    return result;
 }
