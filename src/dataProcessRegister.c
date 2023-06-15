4d65ab419687965a636da561992a66ee8cae0b69
ma3722 <ma3722@ic.ac.uk>
2023-06-15 18:00:41 +0100

    comments to explain funcs in dataProcessRegister.c

--- src/dataProcessRegister.c
+++ src/dataProcessRegister.c
@@ -1,57 +1,67 @@
 #include "dataProcessRegister.h"
 
-
-static void parseArgs(Node node, uint8_t *sf, uint8_t *opr, uint8_t *rm, uint8_t *operand, uint8_t *rn, uint8_t *rd) {
+// processes registers and operand
+static void parseArgs(Node node, uint8_t *sf, uint8_t *shift, uint8_t *rm, uint8_t *operand, uint8_t *rn, uint8_t *rd) {
   parseReg(node -> args[0], sf, rd);
   parseReg(node -> args[1], sf, rn);
   parseReg(node -> args[2], sf, rm);
+  *shift = node -> num <= 3 || !strcmp("lsl", node -> args[3]) ? 0
+      : !strcmp("lsr", node -> args[3]) ? 0b01
+      : !strcmp("asr", node -> args[3]) ? 0b10
+      : 0b11; /*ror*/
   if (node -> num <= 3) {
-    *opr = 0b1000;
     *operand = 0;
-  } else if (!strcmp("lsl", node -> args[3])) {
-    *opr = 0b1000;
-    parseLiteral(node -> args[4], operand);
-  } else if (!strcmp("lsr", node -> args[3])) {
-    *opr = 0b1010;
-    parseLiteral(node -> args[4], operand);
-  } else /*asr*/ {
-    *opr = 0b1100;
+  } else {
     parseLiteral(node -> args[4], operand);
   }
 }
 
-static void composeWord(uint32_t *result, uint8_t *sf, uint8_t *opc, uint8_t *type, uint8_t *opr, uint8_t *rm, uint8_t *operand, uint8_t *rn, uint8_t *rd) {
+// produces the instruction word with given components
+static void composeWord(uint32_t *result, uint8_t sf, uint8_t opc, uint8_t type, uint8_t bit24, uint8_t shift, uint8_t N,uint8_t rm, uint8_t operand, uint8_t rn, uint8_t rd) {
   putBits(result, sf, 31);
   putBits(result, opc, 29);
   putBits(result, type, 25);
-  putBits(result, opr, 21);
+  putBits(result, bit24, 24);
+  putBits(result, shift, 22);
+  putBits(result, N, 21);
   putBits(result, rm, 16);
   putBits(result, operand, 10);
   putBits(result, rn, 5);
   putBits(result, rd, 0);
 }
 
-// Data Processing Register Arithmetic - deals with add, adds, sub, subs
-uint32_t arithmeticReg(Node node) {
+// deals with all arithmetic register and logical cases
+uint32_t arith_or_logic(Node node) {
   uint32_t result = 0;
   uint8_t sf;
   uint8_t opc;
   uint8_t type = 0b0101; /*includes M bit*/
-  uint8_t opr;
+  uint8_t bit24;
+  uint8_t shift;
+  uint8_t N;
   uint8_t rm;
   uint8_t operand;
   uint8_t rn;
   uint8_t rd;
-  if (!strcmp("add", node -> type)) {
+  char *op = node -> type;
+  if (!strcmp("add", op) || !strcmp("and", op) || !strcmp("bic", op)) {
     opc = 0b00;
-  } else if (!strcmp("adds", node -> type)) {
+    bit24 = !strcmp("add", op) ? 1 : 0;
+    N = strcmp("bic", op) ? 0 : 1;
+  } else if (!strcmp("adds", op) || !strncmp("or", op, 2)) {
     opc = 0b01;
-  } else if (!strcmp("sub", node -> type)) {
+    bit24 = !strcmp("adds", op) ? 1 : 0;
+    N = strcmp("orn", op) ? 0 : 1;
+  } else if (!strcmp("sub", op) || !strncmp("eo", op, 2)) {
     opc = 0b10;
-  } else /*subs*/ {
+    bit24 = !strcmp("sub", op) ? 1 : 0;
+    N = strcmp("eon", op) ? 0 : 1;
+  } else /* subs / ands / bics */ {
     opc = 0b11;
+    bit24 = !strcmp("subs", op) ? 1 : 0;
+    N = strcmp("bics", op) ? 0 : 1;
   }
-  parseArgs(node, &sf, &opr, &rm, &operand, &rn, &rd);
-  composeWord(&result, &sf, &opc, &type, &opr, &rm, &operand, &rn, &rd);
+  parseArgs(node, &sf, &shift, &rm, &operand, &rn, &rd);
+  composeWord(&result, sf, opc, type, bit24, shift, N, rm, operand, rn, rd);
   return result;
 }
--- src/dataProcessRegister.h
+++ src/dataProcessRegister.h
@@ -5,7 +5,7 @@
 #include "utils.h"
 
 static void parseArgs(Node, uint8_t *, uint8_t *, uint8_t *, uint8_t *, uint8_t *, uint8_t *);
-static void composeWord(uint32_t *, uint8_t *, uint8_t *, uint8_t *, uint8_t *, uint8_t *, uint8_t *, uint8_t *, uint8_t *);
-extern uint32_t arithmeticReg(Node);
+static void composeWord(uint32_t *, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t);
+extern uint32_t arith_or_logic(Node node);
 
 #endif //ARM_PROJECT_DATAPROCESSREGISTER_H
