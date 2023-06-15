88f5c903537fb67363824ab89de83d5c44dc2f26
ma3722 <ma3722@ic.ac.uk>
2023-06-15 13:12:09 +0100

    wide move and helper functions

--- src/dataProcessImmediate.c
+++ src/dataProcessImmediate.c
@@ -1,15 +1,33 @@
 #include "dataProcessImmediate.h"
 
 // processes registers and operand
-void parseArgs(Node node, uint8_t *sf, uint8_t *sh, uint16_t *imm, uint8_t *rn, uint8_t *rd) {
-  *sh = node -> num > 3 ? 1 : 0;
+void parseArgsArithmetic(Node node, uint8_t *sf, uint8_t *sh, uint16_t *imm, uint8_t *rn, uint8_t *rd) {
+  if (node -> num <= 3) {
+    *sh = 0;
+  } else {
+    uint8_t shift;
+    parseLiteral(node -> args[4], &shift);
+    *sh = shift == 0 ? 0 : 1;
+  }
   parseReg(node -> args[0], sf, rd);
   parseReg(node -> args[1], sf, rn);
   parseLiteral(node -> args[2], imm);
 }
 
+void parseArgsWideMove(Node node, uint8_t *sf, uint8_t *hw, uint16_t *imm, uint8_t *rd) {
+  if (node -> num <= 2) {
+    *hw = 0;
+  } else {
+    uint8_t shift;
+    parseLiteral(node -> args[3], &shift);
+    *hw = shift / 16;
+  }
+  parseReg(node -> args[0], sf, rd);
+  parseLiteral(node -> args[1], imm);
+}
+
 // produces the instruction word with given components
-void composeWord(uint32_t *result, uint8_t sf, uint8_t opc, uint8_t type, uint8_t opi, uint8_t sh, uint16_t imm, uint8_t rn, uint8_t rd) {
+void composeWordArithmetic(uint32_t *result, uint8_t sf, uint8_t opc, uint8_t type, uint8_t opi, uint8_t sh, uint16_t imm, uint8_t rn, uint8_t rd) {
   putBits(result, sf, 31);
   putBits(result, opc, 29);
   putBits(result, type, 26);
@@ -17,6 +35,16 @@ void composeWord(uint32_t *result, uint8_t sf, uint8_t opc, uint8_t type, uint8_
   putBits(result, sh, 22);
   putBits(result, imm, 10);
   putBits(result, rn, 5);
+  putBits(result, rd, 0);
+}
+
+void composeWordWideMove(uint32_t *result, uint8_t sf, uint8_t opc, uint8_t type, uint8_t opi, uint8_t hw, uint16_t imm, uint8_t rd) {
+  putBits(result, sf, 31);
+  putBits(result, opc, 29);
+  putBits(result, type, 26);
+  putBits(result, opi, 23);
+  putBits(result, hw, 21);
+  putBits(result, imm, 5);
   putBits(result, rd, 0);
 }
 
@@ -35,10 +63,30 @@ uint32_t arithmetic(Node node) {
     opc = 0b01;
   } else if (strcmp("sub", node -> type) == 0) {
     opc = 0b10;
-  } else {
+  } else /*subs*/ {
     opc = 0b11;
   }
-  parseArgs(node, &sf, &sh, &imm, &rn, &rd);
-  composeWord(&result, sf, opc, 0b100, opi, sh, imm, rn, rd);
+  parseArgsArithmetic(node, &sf, &sh, &imm, &rn, &rd);
+  composeWordArithmetic(&result, sf, opc, 0b100, opi, sh, imm, rn, rd);
+  return result;
+}
+
+uint32_t wideMove(Node node) {
+  uint32_t result = 0;
+  uint8_t sf;
+  uint8_t opc;
+  uint8_t opi = 0b101;
+  uint8_t hw;
+  uint16_t imm;
+  uint8_t rd;
+  if (strcmp("movn", node -> type) == 0) {
+    opc = 0b00;
+  } else if (strcmp("movz", node -> type) == 0) {
+    opc = 0b10;
+  } else /*movk*/ {
+    opc = 0b11;
+  }
+  parseArgsWideMove(node, &sf, &hw, &imm, &rd);
+  composeWordWideMove(&result, sf, opc, 0b100, opi, hw, imm, rd);
   return result;
 }
--- src/dataProcessImmediate.h
+++ src/dataProcessImmediate.h
@@ -4,8 +4,11 @@
 #include "utils.h"
 #include "structures.h"
 
-extern void parseArgs(Node node, uint8_t *, uint8_t *, uint16_t *, uint8_t *, uint8_t *);
-extern void composeWord(uint32_t *, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint16_t, uint8_t, uint8_t);
+static void parseArgsArithmetic(Node, uint8_t *, uint8_t *, uint16_t *, uint8_t *, uint8_t *);
+static void parseArgsWideMove(Node, uint8_t *, uint8_t *, uint16_t *, uint8_t *);
+static void composeWordArithmetic(uint32_t *, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint16_t, uint8_t, uint8_t);
+static void composeWordWideMove(uint32_t *, uint8_t, uint8_t, uint8_t, uint8_t, uint8_t, uint16_t, uint8_t);
 extern uint32_t arithmetic(Node);
+extern uint32_t wideMove(Node);
 
 #endif //ARM_PROJECT_DATAPROCESSIMMEDIATE_H
