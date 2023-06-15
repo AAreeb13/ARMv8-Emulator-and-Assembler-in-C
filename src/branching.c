6910475bb92fdd85686021cc2a1058479784c1bd
Suweda Abdirahman <sa1722@aube.doc.ic.ac.uk>
2023-06-15 17:02:00 +0100

    tested & corrected all branch funcs, added parseHex func in utils

--- src/branching.c
+++ src/branching.c
@@ -21,29 +21,34 @@ uint32_t unconditionalOffsetA(Node node) {
 // br xn -> branch to address in Xn (??)
 uint32_t unconditionalRegisterA(Node node) {
     uint32_t result;
-    putBits(&result, 0b1101011, 26);
+    putBits(&result, 0b1101011, 25);
     putBits(&result, 0b11111, 16);
-    putBits(&result, parseReg(node.args[0], 0, 0), 5);
+    uint8_t regValue;
+    uint8_t dummySf;
+    parseReg(node.args[0], &dummySf, &regValue);
+    putBits(&result, regValue, 5);
     return result;
 }
 
+
 // b.cond label -> branch to literal iff PSTATE satisfied cond
+// takes in literal address, not the label address
 uint32_t conditionalBranchesA(Node node, char mnemonic[]) {
     // need symbol table to get label address
-    uint32_t labelAddress;
+    uint32_t branchAddress = parseHex(node->args[0]);
     uint32_t result;
     uint8_t cond;
 
-    if (strcmp(mnemonic, "eq")) {cond = 0b0000}
-    else if (strcmp(mnemonic, "ne")) {cond = 0b0001}
-    else if (strcmp(mnemonic, "ge")) {cond = 0b1010}
-    else if (strcmp(mnemonic, "lt")) {cond = 0b1011}
-    else if (strcmp(mnemonic, "gt")) {cond = 0b1100}
-    else if (strcmp(mnemonic, "le")) {cond = 0b1101}
-    else if (strcmp(mnemonic, "al")) {cond = 0b1110}
+    if (strcmp(mnemonic, "eq")) {cond = 0b0000;}
+    else if (strcmp(mnemonic, "ne")) {cond = 0b0001;}
+    else if (strcmp(mnemonic, "ge")) {cond = 0b1010;}
+    else if (strcmp(mnemonic, "lt")) {cond = 0b1011;}
+    else if (strcmp(mnemonic, "gt")) {cond = 0b1100;}
+    else if (strcmp(mnemonic, "le")) {cond = 0b1101;}
+    else if (strcmp(mnemonic, "al")) {cond = 0b1110;}
 
     putBits(&result, 0b10101, 26);
-    putBits(&result, (labelAddress - node.memoryAddress) / 4), 5);
+    putBits(&result, ((branchAddress - node.memoryAddress) / 4), 5);
     putBits(&result, cond, 0);
 
     return result;
--- src/utils.h
+++ src/utils.h
@@ -12,5 +12,6 @@
 extern void putBits(uint32_t *, uint32_t, uint8_t);
 extern void parseReg(char *, uint8_t *, uint8_t *);
 extern void parseLiteral(char *, uint32_t *);
+extern uint32_t parseHex(char *);
 
 #endif //ARM_PROJECT_UTILS_H
