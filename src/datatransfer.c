db62fd25f02aae24c4bf32fdba03a1ca880209a6
Areeb Ahmad <aaa1322@aube.doc.ic.ac.uk>
2023-06-18 18:43:42 +0100

    loading from a literal case is accounted for and all cases now accounted for

--- src/datatransfer.c
+++ src/datatransfer.c
@@ -1,20 +1,16 @@
-//
-// Created by aaa1322 on 14/06/23.
-//
+
 #include <stdio.h>
 #include "datatransfer.h"
 #include "structures.h"
-#include <stdint.h>
-#include <string.h>
 #include "utils.h"
 
-//Pre args = { "Xn || Wn", "[Xm ,..]!" || <literal> || }.
+//Pre args = { "ldr" || "str","Xn || Wn", "[Xm ,..]!" || <literal> || }.
 uint32_t singleDataTransfer(Node instruction) {
   uint8_t totalBitsAdded = 0;
   char **args = instruction->args;
   uint32_t bit30; //also known as sf bit.
   uint32_t rt;
-  parseReg(args[0],&rt,&bit30);// need to remove "x" or "w" //bits 0 to 4.
+  parseReg(args[0],(uint8_t *) &rt,(uint8_t *)&bit30);// need to remove "x" or "w" //bits 0 to 4.
   uint32_t binary = 0; //initialise binary representation of instruction to 0.
   uint32_t bits25to28 = 0b1100;//bits 25 to 28.
   uint32_t isDataTransfer = args[1][0] == '['; //also the bit in position 31 and 29.
@@ -31,16 +27,27 @@ uint32_t singleDataTransfer(Node instruction) {
     putBits(&binary, isLoad, 22); totalBitsAdded++;
     putBits(&binary,0,23); totalBitsAdded++;
 
-    uint32_t xn;parseReg(args[1], &rt, &xn);
+    uint32_t xn;parseReg(args[1], (uint8_t *)&rt,(uint8_t *)&xn);
+    putBits(&binary,xn,5);
     uint32_t offset;
 
-    if (args ==2) {
+    if (instruction->num == 2) { // unsigned offset is 0
       offset = zeroUnsignedOffset();
-
+      putBits(&binary,1,24);
+    }else if (args[2][0] == '#' && args[2][strlen(args[2])-1] == ']'){ //unsigned offset with address code [xn, #<imm>]
+      offset = unsignedImmediateOffset(args);
+    }else if (args[2][0] == '#' && args[2][strlen(args[2])-1] == '!'){
+      offset = preIndexed(args);
+    }else if(args[2][0] == '#'&&
+    args[2][strlen(args[2])-1] != '!'&&
+    args[2][strlen(args[2] - 1)]){
+      offset = postIndexed(args);
+    }else{
+      offset = registerOffset(args);
     }
     putBits(&binary,offset,10);totalBitsAdded+=12; // offset added
   }else{ //Load Literal
-
+    loadLiteral(binary,args);
   }
     return binary;
 }
@@ -60,7 +67,7 @@ uint32_t unsignedImmediateOffset(char **args){
 uint32_t preIndexed(char **args){
   uint32_t offset = 0 + 0b11; //added bits 11, 10
   char strSimm[5];
-  strncpy(strSimm,args[2],strlen(args[2])-2); // provide signed literal in str, e.g "#123" or "#-232"
+  strncpy(strSimm,args[2],strlen(args[2])-2); // provide signed literal in str, e.g "#1234" or "#-1234"
   uint32_t simm9; parseLiteral(strSimm,&simm9);
   simm9 = simm9 & 0b111111111;
   putBits(&offset, simm9, 2);
@@ -80,17 +87,26 @@ uint32_t registerOffset(char **args){
   char xm[5];
   strncpy(xm,args[2],strlen(args[2])-1);
   xm[strlen(args[2])-1] = '\0';
-  uint8_t *redundantsf;
+  //uint8_t *redundantsf;
   uint8_t xmValue;
-  parseReg(xm,redundantsf, &xmValue);
+  parseReg(xm,0, &xmValue);
   putBits(&offset,xmValue,6);
   putBits(&offset, 0b100000011010,0);
   return offset;
 }
-//void loadLiteral(uint32_t binary, char **args){
+void loadLiteral(uint32_t binary, char **args){
+  uint32_t  simm19;
+  if(args[2][0] == '#'){
+    parseLiteral(args[2],&simm19);
+    simm19 = simm19 & 0b1111111111111111111;
+  }else{
+    symbolTable st;
+    simm19 = getAddress(st, args[1]);
+    //label
+  }
+  putBits(&binary,simm19,5);
 
-
-//}
+}
 
 
 
--- src/datatransfer.h
+++ src/datatransfer.h
@@ -6,16 +6,13 @@
 #define SRC_DATATRANSFER_H
 
 #include <stdlib.h>
-#include "structure.h"
-typedef struct Node *Node;
+#include <stdint.h>
+#include <string.h>
 
-struct Node{
-    uint8_t memoryAddress;
-    char *type;
-    uint8_t num;
-    char **args;
-    Node next;
-};
+#include "utils.h"
+#include "structures.h"
+
+
 extern uint32_t singleDataTransfer(Node);
 
 extern uint32_t unsignedImmediateOffset(char **);
@@ -23,6 +20,7 @@ extern uint32_t zeroUnsignedOffset();
 extern uint32_t postIndexed(char **);
 extern uint32_t preIndexed(char **);
 extern uint32_t registerOffset(char **);
+extern void loadLiteral(uint32_t , char **);
 
 
 
