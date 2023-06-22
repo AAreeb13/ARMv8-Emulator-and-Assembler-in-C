a5194ca1f17ca3e8459348d3c15dba314986a7de
Areeb Ahmad <aaa1322@aube.doc.ic.ac.uk>
2023-06-22 19:03:58 +0100

    fixed instruction of format, ldr rn, [xa,xb], and pass 555 tests

--- src/dataTransfer.c
+++ src/dataTransfer.c
@@ -6,6 +6,9 @@
 
 symbolTable mainSymTable2;
 
+uint32_t getRegValue(char *r){
+  return atoi(++r);
+}
 
 void giveSymTableTransfer(symbolTable table) {
   mainSymTable2 = table;
@@ -14,28 +17,29 @@ void giveSymTableTransfer(symbolTable table) {
 //Pre args = { "ldr" || "str","Xn || Wn", "[Xm ,..]!" || <literal> || }.
 uint32_t singleDataTransfer(Node instruction) {
   uint8_t totalBitsAdded = 0;
-  uint32_t bit30; //also known as sf bit.
-  uint32_t rt;
-  parseReg(instruction -> args[0],(uint8_t *) &rt,(uint8_t *)&bit30);// need to remove "x" or "w" //bits 0 to 4.
+  uint32_t bit30 = 0; //also known as sf bit.
+  uint32_t rt = 0;
+  //parseReg(instruction -> args[0],(uint8_t *) &rt,(uint8_t *)&bit30);// need to remove "x" or "w" //bits 0 to 4.
   parseReg(instruction -> args[0],(uint8_t *)&bit30,(uint8_t *)&rt);
   uint32_t binary = 0; //initialise binary representation of instruction to 0.
   uint32_t bits25to28 = 0b1100;//bits 25 to 28.
-  uint32_t isDataTransfer = instruction -> args[1][0] == '['; //also the bit in position 31 and 29.
+  uint32_t isDataTransfer = (instruction -> args[1][0] == '[') ? 1 : 0b0; //also the bit in position 31 and 29.
 
   putBits(&binary,rt,0); totalBitsAdded+=5;
   putBits(&binary,bits25to28,25); totalBitsAdded+=4;
   putBits(&binary,isDataTransfer,29); totalBitsAdded++;
   putBits(&binary,bit30,30); totalBitsAdded++;
-  putBits(&binary, isDataTransfer,31); totalBitsAdded++;
+  putBits(&binary,isDataTransfer,31);totalBitsAdded++;
 
   if (isDataTransfer==1) {// i.e not load from a literal
-    uint32_t isLoad = strcmp(instruction->type , "ldr");//bit in position 22.
+    uint32_t isLoad =!strcmp(instruction->type , "ldr");//bit in position 22.
     putBits(&binary, isLoad, 22); totalBitsAdded++;
     putBits(&binary,0,23); totalBitsAdded++;
 
-    uint32_t xn;parseReg(instruction -> args[1], (uint8_t *)&rt,(uint8_t *)&xn);
-    putBits(&binary,xn,5);
-    uint32_t offset;
+    uint32_t xn=0;
+    parseReg(++(instruction -> args[1]), (uint8_t *)&rt,(uint8_t *)&xn); --(instruction -> args[1]);
+    putBits(&binary,xn,5);totalBitsAdded+=5;
+    uint32_t offset = 0;
 
     if (instruction->num == 2) { // unsigned offset is 0
       offset = zeroUnsignedOffset();
@@ -92,11 +96,11 @@ uint32_t postIndexed(Node instruction){
   return offset;
 }
 uint32_t registerOffset(Node instruction){
-  uint32_t offset;
+  uint32_t offset= 0;
   char xm[5];
   strncpy(xm,instruction -> args[2],strlen(instruction -> args[2])-1);
   xm[strlen(instruction -> args[2])-1] = '\0';
-  uint8_t xmValue;
+  uint8_t xmValue = 0;
   uint8_t redundantsf = 0;
   parseReg(xm,&redundantsf, &xmValue);
   putBits(&offset,xmValue,6);
